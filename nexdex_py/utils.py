b8850a9082704895828083c5bacdb4b940f078bc
OrbDex <letuzinukure67@gmail.com>
2025-12-24 03:36:12 +0700

    Add SDK utility helpers

--- nexdex_py/utils.py
+++ nexdex_py/utils.py
@@ -3,7 +3,7 @@ import time
 
 def time_now_milli_secs() -> int:
     return int(time.time() * 1_000)
-
+ 
 
 def time_now_micro_secs() -> int:
     return int(time.time() * 1_000_000)
@@ -11,4 +11,5 @@ def time_now_micro_secs() -> int:
 
 def raise_value_error(message: str):
     raise ValueError(message)
+
 
