9c769da3eaa3542800725ccc58933512bbea1f19
NexDex <letuzinukure67@gmail.com>
2026-01-21 14:21:47 +0300

    Add SDK utility helpers

--- nexdex_py/utils.py
+++ nexdex_py/utils.py
@@ -1,6 +1,5 @@
 import time
 
-
 def time_now_milli_secs() -> int:
     return int(time.time() * 1_000)
  
@@ -11,5 +10,6 @@ def time_now_micro_secs() -> int:
 
 def raise_value_error(message: str):
     raise ValueError(message)
+
 
 
