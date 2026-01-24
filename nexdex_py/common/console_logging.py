db90ba1e1d4ca743174ee917b544352f82b6432e
NexDex <letuzinukure67@gmail.com>
2026-01-25 01:04:55 +0700

    Add structured console logging

--- nexdex_py/common/console_logging.py
+++ nexdex_py/common/console_logging.py
@@ -4,7 +4,7 @@ import os
 logging.basicConfig(
     level=os.getenv("LOGGING_LEVEL", "INFO"),
     format="%(asctime)s.%(msecs)03d | %(levelname)s | %(message)s", 
-    datefmt="%Y-%m-%d %H:%M:%S",
+    datefmt="%Y-%m-%d %H:%M:%S", 
 )
 
 console_logger = logging.getLogger(__name__)
